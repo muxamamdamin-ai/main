3debc15316093517b1893020cb4c4ef4b31c7876
Muxammadamin <muxamamdamin@gmail.com>
2026-03-04 10:05:17 +0500

    Update models.py

--- main/models.py
+++ main/models.py
@@ -8,6 +8,40 @@ class Students(models.Model):
     is_active = models.BooleanField(default=True)
     birth_date = models.DateField(blank=True,null=True)
 
+    courses = models.ManyToManyField("main.Course", related_name="courses", blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"Student: {self.name} - #{self.id}"
+    
+    def courses_count(self):
+        return self.courses.count()
+    
+    class Meta:
+        verbose_name = "O'quvchi"
+        verbose_name_plural = "O'quvchilar"
+        ordering = ["id"]
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.PositiveIntegerField(default=0)
+    discount_type_choices = (
+        ("flex", "Aniq summa"),
+        ("percent", "Foizli")
+    )
+    discount_type = models.CharField(max_length=10, choices=discount_type_choices)
+    discount = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+    
+    def final_price(self):
+        if self.discount:
+            if self.discount_type == "percent":
+                return self.price - (self.price / 100 * self.discount)
+            return self.price - self.discount
+
+        return self.price
