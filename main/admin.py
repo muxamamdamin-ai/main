685b118dfd188c15adb933278ef872e760132f41
Muxammadamin <muxamamdamin@gmail.com>
2026-03-04 10:06:23 +0500

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,2 +1,13 @@
 from django.contrib import admin
+from main.models import Students, Course
 
+
+admin.site.register(Students)
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "price", "discount", "discount_type", "final_price"]
+    list_display_links = ["title", "id"]
+    list_filter = ["price"]
+    search_fields = ["title"]
+
+admin.site.register(Course, CourseAdmin)
