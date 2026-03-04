d28ab86edd94c7b3a545da5b31dad6938e08fa6d
Muxammadamin <muxamamdamin@gmail.com>
2026-03-04 10:07:31 +0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -31,13 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "unfold",  # <--- Must be at the top
+    "unfold.contrib.filters", # Optional: for better filter styling
+    "unfold.contrib.forms",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'main',
 ]
 
